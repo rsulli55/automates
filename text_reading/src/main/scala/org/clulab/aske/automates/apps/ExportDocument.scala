cc8751de0ccc9054b6e881f08a061f1b3bcc31b8
Maria <maxaalexeeva@gmail.com>
2020-02-21 14:24:50 -0700

    deleted unused methods

--- text_reading/src/main/scala/org/clulab/aske/automates/apps/ExportDocument.scala
+++ text_reading/src/main/scala/org/clulab/aske/automates/apps/ExportDocument.scala
@@ -5,24 +5,17 @@ import java.io.FileWriter
 import java.io.BufferedWriter
 import org.clulab.processors.fastnlp.FastNLPProcessor
 
-import org.clulab.processors.corenlp.CoreNLPProcessor
 import ai.lum.common.ConfigUtils._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.clulab.aske.automates.data.{DataLoader, TextRouter}
 import org.clulab.aske.automates.OdinEngine
 import org.clulab.utils.{DisplayUtils, FileUtils, Serializer}
-import org.clulab.odin.Mention
-import org.clulab.odin.serialization.json.JSONSerializer
-import org.clulab.processors.Document
+
 import org.clulab.serialization.DocumentSerializer
-import org.json4s
-import org.json4s.JValue
-import org.json4s.jackson.JsonMethods._
 
 /**
-  * App used to extract mentions from files in a directory and produce the desired output format (i.e., serialized
-  * mentions or any other format we may need).  The input and output directories as well as the desired export
-  * formats are specified in the config file (located in src/main/resources).
+  * App used to create and export serialized json file of the processors Document
+  * todo: pass file names from config
   * This makes ONE output file for each of the input files.
   */
 object ExtractDoc extends App {
@@ -36,8 +29,6 @@ object ExtractDoc extends App {
   }
 
   val config = ConfigFactory.load()
-
-
   val inputDir = "/home/alexeeva/Desktop/automatesLiteratureToIndex/sci-par-jsons"
   val outputDir = config[String]("apps.outputDirectory")
   val inputType = config[String]("apps.inputType")
@@ -49,38 +40,20 @@ object ExtractDoc extends App {
   val serializer = new DocumentSerializer
   val exporter = new JSONDocExporter()
 
-  //uncomment these for using the text/comment router
-//  val commentReader = OdinEngine.fromConfig(config[Config]("CommentEngine"))
-//  val textRouter = new TextRouter(Map(TextRouter.TEXT_ENGINE -> reader, TextRouter.COMMENT_ENGINE -> commentReader))
 
   // For each file in the input directory:
-  files.par.foreach { file =>
+  files.foreach { file =>
     // 1. Open corresponding output file and make all desired exporters
     println(s"Extracting from ${file.getName}")
     // 2. Get the input file contents
-    // note: for science parse format, each text is a section
+    //here, for the science parse texts, we join all the sections to produce the Document of the whole paper
     val text = dataLoader.loadFile(file).mkString(" ")
-    // 3. Extract causal mentions from the texts
-    // todo: here I am choosing to pass each text/section through separately -- this may result in a difficult coref problem
-//    val mentions = texts.flatMap(reader.extractFromText(_, filename = Some(file.getName)))
-    //The version of mention that includes routing between text vs. comment
-//    val mentions = texts.flatMap(text => textRouter.route(text).extractFromText(text, filename = Some(file.getName))).seq
-
     val document = proc.annotate(text)
-//    serializer.save(document)
+    //jsonify the Document
     val json = document.json(pretty = true)
-//    println(json)
-    println(file.toString())
+    //replace the file name with the name that indicates it has been serialized; todo: need a better way to indictae the target dir
     val newFileName = file.toString().replace(".json", "_seralized").replace("sci-par-jsons", "docs_serialized")
     exporter.export(json, newFileName)
-
-
-//    exportAs.foreach { format =>
-//        val exporter = getExporter(format, s"$outputDir/${file.getName}")
-//        exporter.export(mentions)
-//        exporter.close() // close the file when you're done
-//    }
-
 
 
   }
@@ -97,43 +70,3 @@ case class JSONDocExporter()  {
 
   def close(): Unit = ()
 }
-//trait Exporter {
-//  def export(mentions: Seq[Mention]): Unit
-//  def close(): Unit // for closing the file, if needed
-//}
-
-
-//case class SerializedExporter(filename: String) extends Exporter {
-//  override def export(mentions: Seq[Mention]): Unit = {
-//    Serializer.save[SerializedMentions](new SerializedMentions(mentions), filename + ".serialized")
-//  }
-//
-//  override def close(): Unit = ()
-//}
-//
-//case class JSONExporter(filename: String) extends Exporter {
-//  override def export(mentions: Seq[Mention]): Unit = {
-//    val ast = JSONSerializer.jsonAST(mentions)
-//    val text = compact(render(ast))
-//    val file = new File(filename + ".json")
-//    val bw = new BufferedWriter(new FileWriter(file))
-//    bw.write(text)
-//    bw.close()
-//  }
-//
-//  override def close(): Unit = ()
-//}
-//
-//// Helper Class to facilitate serializing the mentions
-//@SerialVersionUID(1L)
-//class SerializedMentions(val mentions: Seq[Mention]) extends Serializable {}
-//object SerializedMentions {
-//  def load(file: File): Seq[Mention] = Serializer.load[SerializedMentions](file).mentions
-//  def load(filename: String): Seq[Mention] = Serializer.load[SerializedMentions](filename).mentions
-//}
-//
-//
-//object ExporterUtils {
-//
-//  def removeTabAndNewline(s: String) = s.replaceAll("(\\n|\\t)", " ")
-//}
